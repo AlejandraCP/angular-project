01be752f652bf306b7e95bc1514d134a047b1b37
AlejandraCP <alejandracp710@gmail.com>
2018-02-01 23:51:59 -0500

    Agregando module fmorsModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,15 @@ export class AppComponent {
   a = 8;
   b = 5;
   listo = false;
+  // colocar var:string asigna el tipo de dato
+  nombre:string = '';
+// this es la clase
+  constructor() {
+    setTimeout(() => {this.listo = true; }, 3000);
+  }
+// event binding
+  hacerAlgo() {
+    alert('hey');
+    console.log('diste click');
+  }
 }
