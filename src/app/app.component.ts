50dfa9f14fe5018fd5a541d683f87e2543cfd5ab
AlejandraCP <alejandracp710@gmail.com>
2018-02-02 00:15:09 -0500

    usando directivas estructurales ngFor ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,13 +12,23 @@ export class AppComponent {
   listo = false;
   // colocar var:string asigna el tipo de dato
   nombre:string = '';
+  apellido:string = '';
+  lugares:any = [
+    {active: true,
+      nombre: 'Florería La Gardenia'},
+    {active: true,
+      nombre: 'Donas La Pasadita'},
+    {active: false,
+      nombre: 'Veterinaria Huellitas'}
+  ];
 // this es la clase
   constructor() {
-    setTimeout(() => {this.listo = true; }, 3000);
-  }
-// event binding
-  hacerAlgo() {
-    alert('hey');
-    console.log('diste click');
-  }
+//     setTimeout(() => {this.listo = true; }, 3000);
+//   }
+// // event binding
+//   hacerAlgo() {
+//     alert('hey');
+//     console.log('diste click');
+//   }
+
 }
